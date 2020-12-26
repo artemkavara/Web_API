d19d801e67b117de67f15ea89830961a053020a5
Artem Kavara <akavara2000@gmail.com>
2020-12-26 13:59:39 +0200

    Added file and identifiers

---
+++ src/test/java/Web_API/Web_API_Test/Identifiers.java
@@ -0,0 +1,8 @@
+package Web_API.Web_API_Test;
+
+public class Identifiers {
+	static String accessToken = "oCcjtxZ_n9UAAAAAAAAAAZts7hYyqMwJBKi_4nUiqDePylDt6UI6fFxvUk058tws";
+	static String file_meta = "/Get Started with Dropbox.pdf";
+	static String upload_file = "src/file.jpg";
+	static String delete_file = "/file.jpg";
+}
