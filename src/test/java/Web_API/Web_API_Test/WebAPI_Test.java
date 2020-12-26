e35ec0430c099579f87aa3df35ce62fabe4cea58
Artem Kavara <akavara2000@gmail.com>
2020-12-26 14:02:50 +0200

    Added tests

--- src/test/java/Web_API/Web_API_Test/WebAPI_Test.java
+++ src/test/java/Web_API/Web_API_Test/WebAPI_Test.java
@@ -1,20 +1,23 @@
 package Web_API.Web_API_Test;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import static io.restassured.RestAssured.*;
+import static io.restassured.config.EncoderConfig.encoderConfig;
 import org.junit.Test;
+
+import io.restassured.RestAssured;
 
 public class WebAPI_Test 
 {
   
-	String accessToken = "oCcjtxZ_n9UAAAAAAAAAAZts7hYyqMwJBKi_4nUiqDePylDt6UI6fFxvUk058tws";
     @Test
     public void get_meta_data(){
     Map<String, String> path = new HashMap<>();
-    path.put("path", "/Get Started with Dropbox.pdf");
+    path.put("path", Identifiers.file_meta);
     given()
-       .headers("Authorization", "Bearer " + accessToken,
+       .headers("Authorization", "Bearer " + Identifiers.accessToken,
     		   "Content-Type", "application/json")
        .body(path)
        .when()
@@ -24,6 +27,44 @@ public class WebAPI_Test
     }
     @Test
     public void get_list_folders() {
-    	
+    	Map<String, String> path = new HashMap<>();
+        path.put("path", "");
+        given()
+           .headers("Authorization", "Bearer " + Identifiers.accessToken,
+        		   "Content-Type", "application/json")
+           .body(path)
+           .when()
+           .post("https://api.dropboxapi.com/2/files/list_folder")
+           .then()
+           .statusCode(200);	
+    }
+    @Test
+    public void upload_file() {
+    	String dropbox_api_args = "{\"mode\":\"add\",\"autorename\":true,\"mute\":false,\"path\":\"/file.jpg\"}";
+    	RestAssured.config = RestAssured.config().encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false));
+        File file = new File(Identifiers.upload_file);
+    	given()
+        .headers("Authorization", "Bearer " + Identifiers.accessToken,
+     		   "Content-Type", "application/octet-stream", 
+     		   "Dropbox-API-Arg", dropbox_api_args)
+        .body(file)
+    	.when()
+    	.post("https://content.dropboxapi.com/2/files/upload")
+    	.then()
+    	.statusCode(200);
+    }
+    @Test
+    public void delete_file() {
+    	Map<String, String> path = new HashMap<>();
+        path.put("path", Identifiers.delete_file);
+        given()
+        .headers("Authorization", "Bearer " + Identifiers.accessToken,
+     		   "Content-Type", "application/json")
+        .body(path)
+    	.when()
+    	.post("https://api.dropboxapi.com/2/files/delete_v2")
+    	.then()
+    	.statusCode(200);
     }
 }
+
